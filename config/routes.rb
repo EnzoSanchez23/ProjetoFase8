ea00de967e9a1c39f59bc4ae3ad9fb1783df627b
andre-tlima <andre-tlima@hotmail.com>
2019-03-29 19:04:17 +0000

    Adicionando sistema de autenticacao e tokens de usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 require "api_version_constraint"
 
 Rails.application.routes.draw do
-
-  #devise_for :users
-    namespace :api, defaults: {format: :json} do
-      namespace :v1 , path: "/", constraints:ApiVersionConstraint.new(verison: 1, default: true)do
-        resources :users
-      end
+  devise_for :users, controllers: {sessions: "api/v1/sessions"}
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 , path: "/", constraints:ApiVersionConstraint.new(verison: 1, default: true)do
+      resources :users
+      resources :sessions
     end
+  end
 
 end
